9833f412ea97a8b7158342407c6578433d2f9d25
Dmitriev Fedor Yurievitch <fedor_dmitriev@rgs.ru>
2020-08-16 21:06:23 +0300

    finished dict transformation function

--- arina-maplab/src/main/java/arina/maplab/processors/functions/scc/get_field_value.java
+++ arina-maplab/src/main/java/arina/maplab/processors/functions/scc/get_field_value.java
@@ -1,28 +1,33 @@
-package arina.maplab.processors.functions.string;
+package arina.maplab.processors.functions.scc;
 
 import arina.maplab.definitions.IMapComponentDefinition;
 import arina.maplab.processors.contexts.IMapContext;
 import arina.maplab.processors.functions.MapLibraryFunctionProcessor;
 import arina.maplab.value.IMapValue;
 import arina.maplab.value.MapValue;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
-public class dict_transform extends MapLibraryFunctionProcessor {
-    public dict_transform(IMapComponentDefinition definition, Integer growable) {
+public class get_field_value extends MapLibraryFunctionProcessor {
+
+    public get_field_value(IMapComponentDefinition definition, Integer growable) {
         super(definition, growable);
     }
 
     @Override
     protected IMapValue getValueInternal(String index, IMapContext context) throws Exception {
+        // Код справочника, по которому осуществляется поиск
         IMapValue dict_code = computeInputParameter(0, context);
-        IMapValue map_field_ids = computeInputParameter(1, context);
-        IMapValue map_field_values = computeInputParameter(2, context);
-        IMapValue remap_field_id = computeInputParameter(3, context);
+        // Наименование колонки, значение из которой требуется найти
+        IMapValue map_field = computeInputParameter(1, context);
+        // Строковое представление ключ-значение, по которым осуществляется поиск
+        IMapValue remap_field_id = computeInputParameter(2, context);
 
         if (dict_code.isNull())
             return MapValue.NULL;
@@ -33,7 +38,17 @@ public class dict_transform extends MapLibraryFunctionProcessor {
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("POST");
         conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("Content-Type", "text/html");
+        conn.setRequestProperty("Charset", "utf-8");
         conn.setRequestProperty("DictionaryCode", dict_code.getValue(String.class));
+        conn.setRequestProperty("MapField", map_field.getValue(String.class));
+        conn.setDoOutput(true);
+        String value = remap_field_id.getValue(String.class);
+        JSONObject object = new JSONObject(value);
+        OutputStreamWriter wr= new OutputStreamWriter(conn.getOutputStream(), "UTF-8");
+        wr.write(object.toString());
+        wr.flush();
+        wr.close();
         if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
             throw new RuntimeException("Failed : HTTP Error code : "
                     + conn.getResponseCode());
@@ -45,8 +60,8 @@ public class dict_transform extends MapLibraryFunctionProcessor {
             sb.append(output);
         }
         conn.disconnect();
+        in.close();
 
         return new MapValue(this, sb.toString());
-
     }
 }
